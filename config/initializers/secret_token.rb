4a0bce0ea02052a78c9683ae1ad95bac89253e1a
harryj <flamingfurball89@yahoo.com>
2011-02-27 14:56:37 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CalCommons::Application.config.secret_token = '4433bb8637cf59ea7e1652845569896c4204b78fbadee9c250b58ed8c349b6c082c30626885fe2865bb53b5afd9e8f47416bdcfc6ec8190edd22467ed5244200'
